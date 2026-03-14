29cd79f976127136c9c6bf115b938e34853a7862
rencesumangil-hue <rencesumangil@gmail.com>
2026-03-14 17:03:40 +0800

    Add files via upload

---
+++ mlc_tattoo.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2026 at 10:01 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mlc_tattoo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+-- Error reading structure for table mlc_tattoo.bookings: #1932 - Table &#039;mlc_tattoo.bookings&#039; doesn&#039;t exist in engine
+-- Error reading data for table mlc_tattoo.bookings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mlc_tattoo`.`bookings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_messages`
+--
+-- Error reading structure for table mlc_tattoo.chat_messages: #1932 - Table &#039;mlc_tattoo.chat_messages&#039; doesn&#039;t exist in engine
+-- Error reading data for table mlc_tattoo.chat_messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mlc_tattoo`.`chat_messages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table mlc_tattoo.users: #1932 - Table &#039;mlc_tattoo.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table mlc_tattoo.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mlc_tattoo`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
