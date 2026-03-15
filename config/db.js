09401a8e4ae47220a91b0c001a0154979a7e8b29
Rence Sumangil <rencesumangil@gmail.com>
2026-03-15 14:11:05 +0800

    fix railway deploy

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err=>{
-    if(err) throw err;
-    console.log("MySQL Connected");
+  if(err){
+    console.log("Database error:",err);
+  }else{
+    console.log("Database connected");
+  }
 });
 
 module.exports = db;
