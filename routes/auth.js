4c200f49ac39c9df075f794cc8acd4aa537536bb
Rence Sumangil <rencesumangil@gmail.com>
2026-03-27 00:58:19 +0800

    firget

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const db = require('../config/db');
+const axios = require('axios');
 
 /* REGISTER */
 router.post('/register', async (req,res)=>{
@@ -35,9 +36,34 @@ router.post('/register', async (req,res)=>{
 });
 
 /* LOGIN */
-router.post('/login',(req,res)=>{
+router.post('/login', async (req,res)=>{
 
-  const {email,password} = req.body;
+  const {email,password, "g-recaptcha-response": captcha} = req.body;
+
+  // 🔥 CAPTCHA CHECK
+  if(!captcha){
+    return res.redirect('/login.html?error=captcha');
+  }
+
+  try{
+    const verify = await axios.post(
+      "https://www.google.com/recaptcha/api/siteverify",
+      null,
+      {
+        params:{
+          secret:"6LfHXJksAAAAAIf9Q21oSyJiNQYK6ajFV1eQq6BR", // secret key
+          response:captcha
+        }
+      }
+    );
+
+    if(!verify.data.success){
+      return res.redirect('/login.html?error=captcha');
+    }
+
+  }catch(err){
+    return res.redirect('/login.html?error=captcha');
+  }
 
   db.query(
     "SELECT * FROM users WHERE email=?",
@@ -92,6 +118,44 @@ router.get('/logout',(req,res)=>{
   req.session.destroy(()=>{
     res.redirect('/');
   });
+});
+
+router.post('/check-email', (req,res)=>{
+  const {email} = req.body;
+
+  db.query(
+    "SELECT * FROM users WHERE email=?",
+    [email],
+    (err,result)=>{
+
+      if(!result || result.length === 0){
+        return res.redirect('/forgot.html?error=notfound');
+      }
+
+      // 👉 ipapasa natin email sa next page
+      res.redirect(`/reset.html?email=${email}`);
+    }
+  );
+});
+
+
+router.post('/reset-password', async (req,res)=>{
+  const {email,password} = req.body;
+
+  const hashed = await bcrypt.hash(password,10);
+
+  db.query(
+    "UPDATE users SET password=? WHERE email=?",
+    [hashed,email],
+    (err)=>{
+      if(err){
+        console.log(err);
+        return res.send("Error updating password");
+      }
+
+      res.redirect('/login.html?success=reset');
+    }
+  );
 });
 
 module.exports = router;
