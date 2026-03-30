76540a1ef3e63d886675be658765ff8b64ee7fe5
Rence Sumangil <rencesumangil@gmail.com>
2026-03-30 09:04:08 +0800

    Fix firebase config using env

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore();
